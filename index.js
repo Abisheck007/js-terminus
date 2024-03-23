9f7208870ef6f5f6654ce8228502decb8aefafcf
Abisheck <ramaniabira123@gmail.com>
2024-03-23 21:14:38 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let abi = {
+    name: "ABISHeCK",
+    age: 24,
+    gender: "male",
+    
+    siblings: {
+        bro: "aju",
+        sis:"anu"
+    }
+}
 
+abi.siblings.bro="akash"
+
+abi.age=21
+
+console.log(abi.siblings.bro);
+console.log(abi.age);
