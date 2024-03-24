27a4d4dd6c47a20b342663d4833b51460426cf38
Abisheck <ramaniabira123@gmail.com>
2024-03-24 11:26:29 +0530

    created abi.html and abi.css file

--- abi.js
+++ abi.js
@@ -1,3 +1,5 @@
+//this code is for object(here we use curly bracss! and here namma relational depandencies use panikalam)
+
 let abi = {
     name: "ABISHeCK",
     age: 24,
@@ -15,3 +17,10 @@ abi.age=21
 
 console.log(abi.siblings.bro);
 console.log(abi.age);
+
+// this code is for array creation
+
+let san = [ "aju","Anu","deeks"]
+
+console.log(san[1]);
+console.log(san.length)
