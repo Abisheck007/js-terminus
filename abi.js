52bfc6938ebb4174d12349077284d76cb4f1d795
Abisheck <ramaniabira123@gmail.com>
2024-03-28 14:54:18 +0530

    updated abi.js file

--- abi.js
+++ abi.js
@@ -1,5 +1,19 @@
-// using js in-build functions
+// inga namma  mobile buying suggestion concept use panni js code poda porom:Alaparai Starting frnds!:)
 
-var abi = new Date();
+let amount = "10k";
 
-console.log(abi.getHours());
+switch(true){
+    case(amount > "60k"):
+        console.log("buy iphone mobile");
+        break; //imp eppo nammma switch case use panalum namma anga break or  continue use pananum (BL)
+    case(amount > "10k"):
+        console.log("buy android phone");
+        break;
+    case(amount > "5k" && amount < "10k"):
+        console.log("buy button phone");
+        break;
+    default:
+        console.log("u cant afford a phone ryt now!");
+        break;
+
+}
