b3c6c7528d00900fc104ae105680d1c4ef6c4543
Abisheck <ramaniabira123@gmail.com>
2024-08-23 07:01:04 +0530

    our project overview

--- abi.js
+++ abi.js
@@ -1,19 +1,7 @@
-// inga namma  mobile buying suggestion concept use panni js code poda porom:Alaparai Starting frnds!:)
-
-let amount = "10k";
-
-switch(true){
-    case(amount > "60k"):
-        console.log("buy iphone mobile");
-        break; //imp eppo nammma switch case use panalum namma anga break or  continue use pananum (BL)
-    case(amount > "10k"):
-        console.log("buy android phone");
-        break;
-    case(amount > "5k" && amount < "10k"):
-        console.log("buy button phone");
-        break;
-    default:
-        console.log("u cant afford a phone ryt now!");
-        break;
-
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.cta');
+    
+    ctaButton.addEventListener('click', () => {
+        alert('Welcome to the Automated Attendance Management System!');
+    });
+});
